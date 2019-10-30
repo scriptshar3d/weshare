63e42f5280b7a209d6eccd678298a273b19fb421
Ujjwal Wahi <w.ujjwal@gmail.com>
2019-10-30 23:11:37 +0530

    Disable old admin panel routes

--- routes/web.php
+++ routes/web.php
@@ -15,65 +15,65 @@
 /**
  * Auth routes
  */
-Route::group(['namespace' => 'Auth'], function () {
+// Route::group(['namespace' => 'Auth'], function () {
 
-    // Authentication Routes...
-    Route::get('login', 'LoginController@showLoginForm')->name('login');
-    Route::post('login', 'LoginController@login');
-    Route::get('logout', 'LoginController@logout')->name('logout');
-});
+//     // Authentication Routes...
+//     Route::get('login', 'LoginController@showLoginForm')->name('login');
+//     Route::post('login', 'LoginController@login');
+//     Route::get('logout', 'LoginController@logout')->name('logout');
+// });
 
-/**
- * Backend routes
- */
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
-    Route::get('/', 'DashboardController@index')->name('dashboard');
+// /**
+//  * Backend routes
+//  */
+// Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
+//     Route::get('/', 'DashboardController@index')->name('dashboard');
 
-    //Users
-    Route::get('users', 'UserController@index')->name('users');
-    Route::get('users/{user}', 'UserController@show')->name('users.show');
-    Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
-    Route::put('users/{user}', 'UserController@update')->name('users.update');
-    Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+//     //Users
+//     Route::get('users', 'UserController@index')->name('users');
+//     Route::get('users/{user}', 'UserController@show')->name('users.show');
+//     Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
+//     Route::put('users/{user}', 'UserController@update')->name('users.update');
+//     Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
 
-    // Categories
-    Route::get('categories', 'CategoryController@index')->name('categories');
-    Route::get('categories/create', 'CategoryController@create')->name('categories.create');
-    Route::post('categories', 'CategoryController@store')->name('categories.store');
-    Route::get('categories/{category}', 'CategoryController@show')->name('categories.show');
-    Route::get('categories/{category}/edit', 'CategoryController@edit')->name('categories.edit');
-    Route::put('categories/{category}', 'CategoryController@update')->name('categories.update');
-    Route::get('categories/{category}/delete', 'CategoryController@destroy')->name('categories.destroy');
+//     // Categories
+//     Route::get('categories', 'CategoryController@index')->name('categories');
+//     Route::get('categories/create', 'CategoryController@create')->name('categories.create');
+//     Route::post('categories', 'CategoryController@store')->name('categories.store');
+//     Route::get('categories/{category}', 'CategoryController@show')->name('categories.show');
+//     Route::get('categories/{category}/edit', 'CategoryController@edit')->name('categories.edit');
+//     Route::put('categories/{category}', 'CategoryController@update')->name('categories.update');
+//     Route::get('categories/{category}/delete', 'CategoryController@destroy')->name('categories.destroy');
 
-    // Profiles
-    Route::get('profiles', 'UserProfileController@index')->name('profiles');
-    Route::get('profiles/{profile}', 'UserProfileController@show')->name('profiles.show');
-    Route::get('profiles/{profile}/edit', 'UserProfileController@edit')->name('profiles.edit');
-    Route::put('profiles/{profile}', 'UserProfileController@update')->name('profiles.update');
-    Route::get('profiles/{profile}/delete', 'UserProfileController@destroy')->name('profiles.destroy');
+//     // Profiles
+//     Route::get('profiles', 'UserProfileController@index')->name('profiles');
+//     Route::get('profiles/{profile}', 'UserProfileController@show')->name('profiles.show');
+//     Route::get('profiles/{profile}/edit', 'UserProfileController@edit')->name('profiles.edit');
+//     Route::put('profiles/{profile}', 'UserProfileController@update')->name('profiles.update');
+//     Route::get('profiles/{profile}/delete', 'UserProfileController@destroy')->name('profiles.destroy');
 
-    //Posts
-    Route::get('posts', 'PostController@index')->name('posts');
-    Route::get('posts/{post}', 'PostController@show')->name('posts.show');
-    Route::get('posts/{post}/delete', 'PostController@destroy')->name('posts.destroy');
+//     //Posts
+//     Route::get('posts', 'PostController@index')->name('posts');
+//     Route::get('posts/{post}', 'PostController@show')->name('posts.show');
+//     Route::get('posts/{post}/delete', 'PostController@destroy')->name('posts.destroy');
 
-    // Reported Posts
-    Route::get('reports', 'ReportController@index')->name('reports');
-
-
-    //Posts Activity
-    Route::get('activities', 'PostActivityController@index')->name('postactivities');
-    Route::get('activities/{actvity}', 'PostActivityController@show')->name('postactivities.show');
-    Route::get('activities/{actvity}/delete', 'PostActivityController@destroy')->name('postactivities.destroy');
-
-    //Comments
-    Route::get('comments', 'CommentController@index')->name('comments');
-    Route::get('comments/{comment}', 'CommentController@show')->name('comments.show');
-    Route::get('comments/{comment}/delete', 'CommentController@destroy')->name('comments.destroy');
-
-    //Comments Activity
-    Route::get('comment/activities', 'UserController@index')->name('commentsactivities');
-});
+//     // Reported Posts
+//     Route::get('reports', 'ReportController@index')->name('reports');
 
 
-Route::get('/', 'Auth\LoginController@showLoginForm');
+//     //Posts Activity
+//     Route::get('activities', 'PostActivityController@index')->name('postactivities');
+//     Route::get('activities/{actvity}', 'PostActivityController@show')->name('postactivities.show');
+//     Route::get('activities/{actvity}/delete', 'PostActivityController@destroy')->name('postactivities.destroy');
+
+//     //Comments
+//     Route::get('comments', 'CommentController@index')->name('comments');
+//     Route::get('comments/{comment}', 'CommentController@show')->name('comments.show');
+//     Route::get('comments/{comment}/delete', 'CommentController@destroy')->name('comments.destroy');
+
+//     //Comments Activity
+//     Route::get('comment/activities', 'UserController@index')->name('commentsactivities');
+// });
+
+
+Route::get('/', 'HomeController@index');
