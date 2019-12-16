163d37b3d524ba9ddae2cf33f34147c107b1c039
Ujjwal Wahi <w.ujjwal@gmail.com>
2019-12-16 19:06:38 +0530

    Show older stories first

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -179,7 +179,7 @@ class PostController extends Controller
     {
         $posts = Post::where('user_profile_id', $userProfile->id)->where('is_story', true)
             ->where('created_at', '>', Carbon::now()->subHours(24))
-            ->orderBy('created_at', 'desc')->get();
+            ->orderBy('created_at', 'asc')->get();
         return response()->json($posts);
     }
 
